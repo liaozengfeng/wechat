00a8da728074c11941285cf16259f77e2c4b785d
liaozengfeng <916592845@qq.com>
2019-10-15 09:32:21 +0800

    six

--- app/Http/Controllers/Admin/ExecController.php
+++ app/Http/Controllers/Admin/ExecController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class ExecController extends Controller
 {
     public function exec(Request $request){
+        //接收微信发送的信息
         $info=file_get_contents("php://input");
-        dd($info);
+        //写入log日志
         file_put_contents(storage_path('logs/shop/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
         file_put_contents(storage_path('logs/shop/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
+        //处理xml的加密
         $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
         $xml_arr = (array)$xml_obj;
         dd($xml_arr);
