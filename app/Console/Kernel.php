518371ec8548723a00061f25b04360c35752b13c
liaozengfeng <916592845@qq.com>
2019-10-23 20:01:20 +0800

    212312

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,53 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-
+            $user_info=IntegralModel::get()->toArray();
+            $data=CourseModel::get()->toArray();
+            $url="https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=".access_token();
+            foreach ($user_info as $ke=>$va){
+                if (!empty($va['one'])){
+                    $arr['name']=$va['name'];
+                    $arr['openid']=$va['openid'];
+                    foreach($data as $k=>$v) {
+                        if ($va['one'] == $v['id']) {
+                            $arr['one'] = $v['name'];
+                        } else if ($va['two'] == $v['id']) {
+                            $arr['two'] = $v['name'];
+                        } else if ($va['three'] == $v['id']) {
+                            $arr['three'] = $v['name'];
+                        } else if ($va['four'] == $v['id']) {
+                            $arr['four'] = $v['name'];
+                        }
+                    }
+                    $aaa=[
+                        "touser"=>$arr['openid'],
+                        "template_id"=>" 	UCvamyRtD2jXBN_jxOVbbWaO0H_7OP26qwvltPALbac",
+                        "data"=>[
+                            "name"=>[
+                                "value"=>$arr['name'],
+                                "color"=>"red",
+                            ],"one"=>[
+                                "value"=>$arr['one'],
+                                "color"=>"red",
+                            ],"two"=>[
+                                "value"=>$arr['two'],
+                                "color"=>"red",
+                            ],"three"=>[
+                                "value"=>$arr['three'],
+                                "color"=>"red",
+                            ],"four"=>[
+                                "value"=>$arr['four'],
+                                "color"=>"red",
+                            ],
+                        ]
+                    ];
+                    $aaa=json_encode($aaa,JSON_UNESCAPED_UNICODE);
+                    $re=curl_post($url,$aaa);
+                    $re=json_decode($re,1);
+                    file_put_contents(storage_path('logs/shop/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
+                    file_put_contents(storage_path('logs/shop/'.date('Y-m-d').'.log'),$re,FILE_APPEND);
+                }
+            }
         })->dailyAt('20:00');;
     }
 
@@ -37,53 +83,6 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-        $user_info=IntegralModel::get()->toArray();
-        $data=CourseModel::get()->toArray();
-        $url="https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=".access_token();
-        foreach ($user_info as $ke=>$va){
-            if (!empty($va['one'])){
-                $arr['name']=$va['name'];
-                $arr['openid']=$va['openid'];
-                foreach($data as $k=>$v) {
-                    if ($va['one'] == $v['id']) {
-                        $arr['one'] = $v['name'];
-                    } else if ($va['two'] == $v['id']) {
-                        $arr['two'] = $v['name'];
-                    } else if ($va['three'] == $v['id']) {
-                        $arr['three'] = $v['name'];
-                    } else if ($va['four'] == $v['id']) {
-                        $arr['four'] = $v['name'];
-                    }
-                }
-                $aaa=[
-                    "touser"=>$arr['openid'],
-                    "template_id"=>" 	UCvamyRtD2jXBN_jxOVbbWaO0H_7OP26qwvltPALbac",
-                    "data"=>[
-                        "name"=>[
-                            "value"=>$arr['name'],
-                            "color"=>"red",
-                            ],"one"=>[
-                            "value"=>$arr['one'],
-                            "color"=>"red",
-                        ],"two"=>[
-                            "value"=>$arr['two'],
-                            "color"=>"red",
-                        ],"three"=>[
-                            "value"=>$arr['three'],
-                            "color"=>"red",
-                        ],"four"=>[
-                            "value"=>$arr['four'],
-                            "color"=>"red",
-                        ],
-                    ]
-                ];
-                $aaa=json_encode($aaa,JSON_UNESCAPED_UNICODE);
-                $re=curl_post($url,$aaa);
-                $re=json_decode($re,1);
-                file_put_contents(storage_path('logs/shop/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
-                file_put_contents(storage_path('logs/shop/'.date('Y-m-d').'.log'),$re,FILE_APPEND);
-            }
-        }
         require base_path('routes/console.php');
     }
 }
