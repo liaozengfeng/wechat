e54b816b5f25b998f15972b8ff4537d639019e69
liaozengfeng <916592845@qq.com>
2019-12-11 18:12:56 +0800

    456

--- app/Http/Controllers/Port/PortController.php
+++ app/Http/Controllers/Port/PortController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 class PortController extends Controller
 {
     public function index(Request $request){
-        echo 111;exit;
         var_dump($request->all());exit;
         $redis = new \Redis();
         $redis->connect('127.0.0.1');
