f2ab2051e94d573ae4cd47626db4b632fbfd50f6
liaozengfeng <916592845@qq.com>
2019-12-11 18:29:52 +0800

    456

--- app/Http/Controllers/Port/PortController.php
+++ app/Http/Controllers/Port/PortController.php
@@ -7,8 +7,23 @@ use App\Http\Controllers\Controller;
 class PortController extends Controller
 {
     public function index(Request $request){
-        var_dump($request->all());exit;
+        $info=$request->all();
         $redis = new \Redis();
         $redis->connect('127.0.0.1');
+        if(empty($redis->get('shopinfo'))){
+            $data[]=$info;
+            $redis->set("shopinfo",json_encode($data,1),60*20);
+        }else{
+            $data=json_decode($redis->get('shopinfo'),1);
+            $data[]=$info;
+            $redis->set("shopinfo",json_encode($data,1),60*20);
+        }
+        return 1;
+    }
+
+    public function list(Request $request){
+        $redis = new \Redis();
+        $redis->connect('127.0.0.1');
+        return json_decode($redis->get("shopinfo"),1);
     }
 }
